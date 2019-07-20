46e3be6920f88281f0b08814c2728671b9083b93
Alexander Mills <alex@alexs-mac.local>
2019-07-19 18:02:44 -0700

    tmp

--- test/simple.js
+++ test/simple.js
@@ -21,11 +21,11 @@ const z = {
   typings: 'dist/main.d.ts',
   scripts: {test: 'suman test', postinstall: './assets/postinstall.sh', xyz: './scripts/npm/xyz.sh'},
   repository: {type: 'git', url: 'git+https://github.com/ORESoftware/typescript-library-skeleton.git'},
-  keywords: ['typescript', 'library', 'skeleton', 'scaffold'],
+  keywords: ['typescript', 'library', 'skeleton', 'scaffold', {roomba: 'chicken'}],
   author: 'YOUR NAME HERE',
   license: 'SEE LICENSE IN license.md',
   bugs: {url: 'https://github.com/ORESoftware/typescript-library-skeleton/issues'},
   homepage: 'https://github.com/ORESoftware/typescript-library-skeleton#readme',
   dependencies: {},
   devDependencies: {'@types/node': '^10.12.10', 'typescript': '^3.4.5'}
-};
+}
